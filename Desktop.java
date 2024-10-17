fcd52e025ad1868b406e0370b94ea52e43438d7c
Mitchell Kelm <mitchell.kelm@icloud.com>
2024-10-17 11:22:57 -0600

    Completed task 3 (Rewrite Desktop Class for Composition and Immutablity

--- Desktop.java
+++ Desktop.java
@@ -1,34 +1,31 @@
 //Desktop computer: adds GPU type
 
-public class Desktop extends Computer { //Inherits from Computer
-    String GPUType=null;
+public class Desktop {
+    private final Computer computer; //Composition: Desktop has a Computer
+    private final String GPUType;
 
     //Constructors
-    public Desktop() {} //No-arg constructor
+    public Desktop(Computer computer, String GPUType) {
+        this.computer = computer;
+        this.GPUType = GPUType;
 
-    public Desktop(String CPU, String RAM, String disk, String GPUType) {
-        //Inherited from Computer superclass
-        this.CPU=CPU;
-        this.RAM=RAM;
-        this.disk=disk;
-
-        //Only in Desktop subclass
-        this.GPUType=GPUType;
-    }
-
-    //Setter
-    public void setGPUType(String GPUType) {
-        this.GPUType=GPUType;
     }
 
     //Getter
+    public Computer getComputer() {
+        return computer;
+    }
+    
+
+    //Getter
     public String getGPUType() {
-        return this.GPUType;
+        return GPUType;
     }
 
     //Return formatted version of data
     public String toString() {
-        return "Type:Desktop\tCPU:" + this.CPU + "\tRAM:" + this.RAM + "\tDisk:" + this.disk + "\tGPU:" + this.GPUType;
+        return "Type: Desktop\tCPU: " + computer.getCPU() + "\tRAM: " + computer.getRAM() + 
+               "\tDisk: " + computer.getDisk() + "\tGPU: " + GPUType;
     }
 
 }
